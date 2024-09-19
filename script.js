a71f9405733dd4a0153d8941a657313327bd249c
Kaua <rodrigueskaua76@gmail.com>
2024-09-19 11:15:12 -0300

    Criando as variaveis definidas, indefinidas e vazia

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 //DECLARANDO AS VARIAVEIS
 
+//declaração definida
 var nome ="fiap";
 console.log(nome)
 
@@ -10,3 +11,13 @@ console.log(nome1)
 
 const nome2 = "A"
 console.log(nome2)
+
+//outras declaraçoes
+
+//declaração indefinida
+let nome3;
+console.log(nome3)
+
+//declaração vazia
+let disciplina =null
+console.log(disciplina)
