e2d13027db285d4d935838169c7fbe6b23b22b2f
Kaua <rodrigueskaua76@gmail.com>
2024-09-19 11:08:01 -0300

    Declarando os tipos de dados, var, let e const

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Olá mundo-externo")
+// alert("Olá mundo-externo")
+
+//DECLARANDO AS VARIAVEIS
+
+var nome ="fiap";
+console.log(nome)
+
+let nome1 ="cidade"
+console.log(nome1)
+
+const nome2 = "A"
+console.log(nome2)
